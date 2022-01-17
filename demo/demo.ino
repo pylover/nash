ad065968c27a17b3a769e1e53cff93ef546d1b5c
pylover <vahid.mardani@gmail.com>
2022-01-17 03:32:40 +0330

    Standard input and eof

--- demo/demo.ino
+++ demo/demo.ino
@@ -39,12 +39,34 @@ int cat(size_t argc, char **argv, struct process *self) {
 	return ALIVE;
 }
 
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
+}
+
+int free(size_t argc, char **argv, struct process *self) {
+	PRINTLN(freeMemory());
+}
 
 static struct executable programs[] = {
 	{"help", nash_help },
 	{"echo", echo },
 	{"sleep", sleep },
 	{"cat", cat },
+	{"free", free },
 	{ NULL }
 };
 
